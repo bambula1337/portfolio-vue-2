da1bc0ee6fa36c207dc85098ed1f2e03c46a5836
Adm's <lemopidr1@gmail.com>
2022-04-17 14:42:20 +0200

    Added tailwind sccs

--- portfoliovue2/src/App.vue
+++ portfoliovue2/src/App.vue
@@ -2,11 +2,16 @@
   <div id="app">
     <div class="main">
       <router-view />
+      <p>asdkasdaksdjad</p>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+// Tailwind Connection
+@import "./assets/style/tailwind.scss";
+
 #app {
 }
+
 </style>
