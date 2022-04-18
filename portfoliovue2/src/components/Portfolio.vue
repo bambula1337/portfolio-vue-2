dc5415dd67560c8ed74062b30a34f3dffbc3ee8c
Adm's <lemopidr1@gmail.com>
2022-04-18 15:06:30 +0200

    Trying to connect Swiper js

---
+++ portfoliovue2/src/components/Portfolio.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="portfolio">
+    <WelcomeText v-bind="WelcomeTextProps"/>
+    <div class="portfolio-main">
+      
+    </div>
+  </div>  
+</template>
+
+<script>
+import WelcomeText from '@/components/WelcomeText.vue';
+
+export default {
+  name: 'Portfolio',
+  components: {
+    WelcomeText,
+  },
+  data(){
+    return{
+      WelcomeTextProps: {
+        mainText: "Services",
+        subText: "What I do",
+      },
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.portfolio{
+  @apply mb-20;
+}
+</style>
